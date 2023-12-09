1bba92ede547d7efadf020af61ee802c6534a57d
Mickaël Schoentgen <contact@tiger-222.fr>
2023-12-09 01:01:16 +0100

    fix: Adapt test_no_args()

--- src/tests/test_entry_point.py
+++ src/tests/test_entry_point.py
@@ -2,8 +2,11 @@
 This is part of the inverter COM Python's module.
 Source: https://github.com/BoboTiG/python-inverter-com
 """
+from unittest.mock import patch
+
 from inverter_com.__main__ import main
 
 
 def test_no_args() -> None:
-    assert main() == 0
+    with patch("sys.argv", []):
+        assert main() == 0
