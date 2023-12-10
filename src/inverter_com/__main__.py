1945093d311ab54eeaa89e908025aea0149facd6
Mickaël Schoentgen <contact@tiger-222.fr>
2023-12-10 21:47:09 +0100

    feat: Rename execuable `inverter-com` -> `inverter-read`
    As I might add another `inverter-send` command to control the inverter.

--- src/inverter_com/__main__.py
+++ src/inverter_com/__main__.py
@@ -2,24 +2,19 @@
 This is part of the inverter COM Python's module.
 Source: https://github.com/BoboTiG/python-inverter-com
 """
-import sys
 from argparse import ArgumentParser
 
 
-def main(*args: str) -> int:
+def main() -> int:
     parser = ArgumentParser(prog="inverter-com")
     parser.add_argument("--port", default="/dev/ttyUSB0", help="serial port device")
     parser.add_argument("--debug", action="store_true", help="enable debug logging")
-    parser.add_argument("commands", nargs="+", help="commands to send to the inverter")
-    options = parser.parse_args(args or None)
+    parser.add_argument("command", nargs="+", help="command to send to the inverter")
+    options = parser.parse_args()
 
-    if options.commands:
+    if options.command:
         from inverter_com.helpers import test_commands
 
-        test_commands(options.port, *options.commands, debug=options.debug)
+        test_commands(options.port, *options.command, debug=options.debug)
 
     return 0
-
-
-if __name__ == "__main__":  # pragma: nocover
-    sys.exit(main())
--- src/tests/test_entry_point.py
+++ src/tests/test_entry_point.py
@@ -7,15 +7,7 @@ from unittest.mock import patch
 from inverter_com.__main__ import main
 
 
-def test_command_from_func_args(port: str) -> None:
-    with patch("inverter_com.inverter.Inverter.send") as mocked:
-        mocked.return_value = {"foo": 42}
-        assert main("cmd", "--port", port) == 0
-        assert mocked.call_count == 2
-        mocked.assert_called_with("cmd")
-
-
-def test_command_from_func_sys_argv(port: str) -> None:
+def test_single_command(port: str) -> None:
     with (
         patch("sys.argv", ["inverter-com", "cmd", "--port", port]),
         patch("inverter_com.inverter.Inverter.send") as mocked,
@@ -26,16 +18,7 @@ def test_command_from_func_sys_argv(port: str) -> None:
         mocked.assert_called_with("cmd")
 
 
-def test_commands_from_func_args(port: str) -> None:
-    with patch("inverter_com.inverter.Inverter.send") as mocked:
-        mocked.return_value = {"foo": 42}
-        assert main("cmd1", "cmd2", "--port", port) == 0
-        assert mocked.call_count == 3
-        mocked.assert_any_call("cmd1")
-        mocked.assert_any_call("cmd2")
-
-
-def test_commands_from_func_sys_argv(port: str) -> None:
+def test_multiple_commands(port: str) -> None:
     with (
         patch("sys.argv", ["inverter-com", "cmd1", "cmd2", "--port", port]),
         patch("inverter_com.inverter.Inverter.send") as mocked,
