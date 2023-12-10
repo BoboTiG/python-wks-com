ee4879ec46fd73ca4cee8fac3700d7159d0b864b
Mickaël Schoentgen <contact@tiger-222.fr>
2023-12-10 22:44:31 +0100

    fix: Actually rename `inverter-com` -> `inverter-read`

--- src/inverter_com/__main__.py
+++ src/inverter_com/__main__.py
@@ -5,8 +5,8 @@ Source: https://github.com/BoboTiG/python-inverter-com
 from argparse import ArgumentParser
 
 
-def main() -> int:
-    parser = ArgumentParser(prog="inverter-com")
+def read() -> int:
+    parser = ArgumentParser(prog="inverter-read")
     parser.add_argument("--port", default="/dev/ttyUSB0", help="serial port device")
     parser.add_argument("--debug", action="store_true", help="enable debug logging")
     parser.add_argument("command", nargs="+", help="command to send to the inverter")
--- src/tests/test_entry_point.py
+++ src/tests/test_entry_point.py
@@ -4,27 +4,27 @@ Source: https://github.com/BoboTiG/python-inverter-com
 """
 from unittest.mock import patch
 
-from inverter_com.__main__ import main
+from inverter_com import __main__
 
 
-def test_single_command(port: str) -> None:
+def test_read_single_command(port: str) -> None:
     with (
-        patch("sys.argv", ["inverter-com", "cmd", "--port", port]),
+        patch("sys.argv", ["inverter-read", "cmd", "--port", port]),
         patch("inverter_com.inverter.Inverter.send") as mocked,
     ):
         mocked.return_value = {"foo": 42}
-        assert main() == 0
+        assert __main__.read() == 0
         assert mocked.call_count == 2
         mocked.assert_called_with("cmd")
 
 
-def test_multiple_commands(port: str) -> None:
+def test_read_multiple_commands(port: str) -> None:
     with (
-        patch("sys.argv", ["inverter-com", "cmd1", "cmd2", "--port", port]),
+        patch("sys.argv", ["inverter-read", "cmd1", "cmd2", "--port", port]),
         patch("inverter_com.inverter.Inverter.send") as mocked,
     ):
         mocked.return_value = {"foo": 42}
-        assert main() == 0
+        assert __main__.read() == 0
         assert mocked.call_count == 3
         mocked.assert_any_call("cmd1")
         mocked.assert_any_call("cmd2")
