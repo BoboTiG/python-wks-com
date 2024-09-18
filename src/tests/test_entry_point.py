e0b443e24711fbcb0f82af446a74e5b178b03210
Mickaël Schoentgen <contact@tiger-222.fr>
2024-09-18 13:10:58 +0200

    feat(tests): rework entry point tests + full coverage

--- src/tests/test_entry_point.py
+++ src/tests/test_entry_point.py
@@ -3,29 +3,45 @@ This is part of the inverter COM Python's module.
 Source: https://github.com/BoboTiG/python-wks-com
 """
 
+import json
 from unittest.mock import patch
+
+import pytest
 
 from wks_com import __main__
 
+MOCKED_RETURN_VALUE = {"foo": 42}
+FORMATTED_RETURN_VALUE = json.dumps(MOCKED_RETURN_VALUE, indent=4, sort_keys=True)
 
-def test_read_single_command(port: str) -> None:
-    with (
-        patch("sys.argv", ["wks-read", "cmd", "--port", port]),
-        patch("wks_com.inverter.Inverter.send") as mocked,
-    ):
-        mocked.return_value = {"foo": 42}
+
+@pytest.mark.parametrize("debug", [True, False])
+@pytest.mark.parametrize(
+    "commands",
+    [
+        ["cmd"],
+        ["cmd1", "cmd2"],
+    ],
+)
+def test_read(
+    commands: list[str], debug: bool, port: str, capsys: pytest.CaptureFixture, caplog: pytest.LogCaptureFixture
+) -> None:
+    full_cmd = ["wks-read", "--port", port, *commands]
+    if debug:
+        full_cmd.append("--debug")
+
+    with patch("sys.argv", full_cmd), patch("wks_com.inverter.Inverter.send") as mocked:
+        mocked.return_value = MOCKED_RETURN_VALUE
         assert __main__.read() == 0
-        assert mocked.call_count == 1
-        mocked.assert_called_with("cmd")
+        assert mocked.call_count == len(commands)
+        for command in commands:
+            mocked.assert_any_call(command)
 
+    expected_output = "\n".join(f"{command}\n{FORMATTED_RETURN_VALUE}\n" for command in commands)
+    assert capsys.readouterr().out.strip() == expected_output.strip()
 
-def test_read_multiple_commands(port: str) -> None:
-    with (
-        patch("sys.argv", ["wks-read", "cmd1", "cmd2", "--port", port]),
-        patch("wks_com.inverter.Inverter.send") as mocked,
-    ):
-        mocked.return_value = {"foo": 42}
-        assert __main__.read() == 0
-        assert mocked.call_count == 2
-        mocked.assert_any_call("cmd1")
-        mocked.assert_any_call("cmd2")
+    if debug:
+        # As the logging level was lowered to DEBUG, we should see semothing here.
+        # Maybe related to https://github.com/pytest-dev/pytest/issues/9393?
+        assert not caplog.records
+    else:
+        assert not caplog.records
